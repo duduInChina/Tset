330ccff9ce3364cb6e37d86eb0f3e762be0800b3
duduInChina <394043555@qq.com>
2015-04-14 11:42:07 +0800

    dudu

--- MyApplication/app/src/main/java/com/example/zicong/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/zicong/myapplication/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         System.out.print("sdafasdsfgsdffasdf");
+        System.out.print("sdafasdsfgsdffasdf");
+        System.out.print("sdafasdsfgsdffasdf");
 
     }
 
